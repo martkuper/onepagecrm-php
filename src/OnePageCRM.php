eec5ecf65e2a9b105accd8bde3529f4d82c49df9
Mart Kuper <martkuper@hotmail.com>
2015-02-17 10:08:39 +0100

    Added login() function and authenticate() function

--- src/OnePageCRM.php
+++ src/OnePageCRM.php
@@ -42,85 +42,107 @@ class OnePageCRM
 			]);
 		}	
 
-		$this->setGuzzleClient($client);
-		$this->setConfig($config);
+		$this->guzzleClient = $client;
+		$this->config       = $config;
+
+		if(!$config->getAuthKey() || !$config->getUserId()) {
+			$this->login();	
+		}		
 	}
 
 	/**
 	 * Use the 'POST' method to send data to OnePageCRM
 	 *
 	 * The url you enter here, will be appended to the base url.
-	 * For example: post('login.json', $data, $partial) 
+	 * 
+	 * For example: post('login.json', $data) 
+	 * 
 	 * will post to 'https://app.onepagecrm.com/api/v3/login.json'
 	 * unless another base url is supplied.
-	 * The body must be supplied as an associative array.
-	 * For example post($url, ['login' => 'e@mail.com', 'password' => 'pass'])
-	 * Setting partial to true (default) will allow you to send a selection of the default fields
-	 * wile setting the other fields to null. 
-	 * For example: you can add a new contact with only a name and email address,
-	 * all other field will be set to null.
 	 * 
-	 * @param  string  $url    	The url to append to the base url
-	 * @param  array   $body    The data to send
-	 * @return boolean $partial Send a partial request
+	 * The body must be supplied as an associative array.
+	 * 
+	 * For example: post($url, ['login' => 'e@mail.com', 'password' => 'pass'])
+	 * 
+	 * @param  string   $url     The url to append to the base url
+	 * @param  array    $body    The data to send
+	 * @return Response          GuzzleHttp\Response object
 	 */
 	public function post($url , $body)
 	{
 		$data['json'] = $body;
-		
-		$client = $this->getGuzzleClient();
+
+		// TODO: Explain
+		if(!isset($body['login']) && !isset($body['password'])) {
+			$headers = $this->authenticate($body, $url);
+			$headers['Content-Type'] = 'application/json';
+			$data['headers'] = $headers;
+		}
+
+		$client = $this->guzzleClient;
 		$request = $client->createRequest('POST', $url, $data);
-		
 		$response = $client->send($request);
+		
 		return $response;
 	}
 
 	/**
-	 * Authenticates to the OnePageCRM API
-	 * 
-	 * TODO: complete PHPDoc and complete function
-	 * TODO: change function visibility to private
-	 * 
+	 * TODO: Update documentation
+	 * [login description]
 	 * @return [type] [description]
 	 */
-	public function authenticate()
+	public function login()
 	{
+		$config = $this->config;
+
+		$response = $this->post('login.json', [
+			'login' => $config->getEmail(), 
+			'password' => $config->getPassword()
+		]);
+		$response_json = $response->json();
 		
+		$uid = $response_json['data']['user_id'];
+		$key = base64_decode($response_json['data']['auth_key']);
+
+		$config->setUserId($uid);
+		$config->setAuthKey($key);
+
+		return $response;
 	}
 
 	/**
-	 * Gets the guzzleClient
-	 * @return GuzzleHttp\Client The GuzzleHttp\Client object
+	 * Generate OnePageCRM authentication keys
+	 * 
+	 * TODO: update documentation
+	 * 
+	 * @return array Array of HTTP headers
 	 */
-	public function getGuzzleClient()
+	public function authenticate($data, $url, $http_method = 'POST')
 	{
-		return $this->guzzleClient;
-	}
+		$config = $this->config;
+		$uid = $config->getUserId();
+		$key = $config->getAuthKey();
+		
+		$full_url = $config->getBaseUrl() . $url;
+		$timestamp = time();
+		$auth_data = array($uid, $timestamp, $http_method, sha1($full_url));
+		$request_headers = array();
 
-	/**
-	 * Get the configuration object
-	 * @return Config The configuration object
-	 */
-	public function getConfig()
-	{
-		return $this->config;
-	}
+		if($http_method == 'POST' || $http_method == 'PUT') {
+			$json_data = json_encode($data);
+			$auth_data[] = sha1($json_data);
+		}
+		
+		if($key != null && $uid != null) {
+			$hash = hash_hmac('sha256', implode('.', $auth_data), $key);
+			$request_headers["X-OnePageCRM-UID"] = $uid;
+			$request_headers["X-OnePageCRM-TS"] = $timestamp;
+			$request_headers["X-OnePageCRM-Auth"] = $hash;
+		} else {
+			$this->login();
+			return $this->authenticate($data, $url, $http_method);
+		}
 
-	/**
-	 * Sets the guzzleClient
-	 * @param GuzzleHttp\Client $guzzleClient The GuzzleHttp\Client object
-	 */
-	public function setGuzzleClient(Client $guzzleClient)
-	{
-		$this->guzzleClient = $guzzleClient;
-	}
-
-	/**
-	 * Set the configuration object
-	 * @param Config $config The configuration object
-	 */
-	public function setConfig(Config $config)
-	{
-		$this->config = $config;
+		return $request_headers;
 	}
 }
